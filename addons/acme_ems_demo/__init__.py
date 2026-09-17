70998e66879c77a46e3140d7191213984a90abb4
thallukrish <thallukrish@gmail.com>
2026-09-17 10:50:38 +0530

    fix: keep manifest hook implementation in hooks module

--- addons/acme_ems_demo/__init__.py
+++ addons/acme_ems_demo/__init__.py
@@ -1,7 +1,1 @@
-from .hooks import post_init_hook as _base_post_init_hook
-from .scenario_extension import extend_demo_scenario
-
-
-def post_init_hook(env):
-    _base_post_init_hook(env)
-    extend_demo_scenario(env)
+from .hooks import post_init_hook
