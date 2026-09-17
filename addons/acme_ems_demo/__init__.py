da4f2d0824c43d6a67eb0b637cd238d4cbeb3f0f
thallukrish <thallukrish@gmail.com>
2026-09-17 10:50:07 +0530

    feat: extend ACME demo seed scenario

--- addons/acme_ems_demo/__init__.py
+++ addons/acme_ems_demo/__init__.py
@@ -1,1 +1,7 @@
-from .hooks import post_init_hook
+from .hooks import post_init_hook as _base_post_init_hook
+from .scenario_extension import extend_demo_scenario
+
+
+def post_init_hook(env):
+    _base_post_init_hook(env)
+    extend_demo_scenario(env)
